9d4ba36e44744acb7fdb03f680c97ab26c3aa701
kristaps <>
2017-03-27 10:18:30 +0000

    Add dependency tracking in linkage phase.

--- linker.c
+++ linker.c
@@ -2,6 +2,7 @@
 
 #include <sys/queue.h>
 
+#include <assert.h>
 #if HAVE_ERR
 # include <err.h>
 #endif
@@ -61,11 +62,58 @@ checktarget(struct ref *ref, struct strctq *q)
 	return(0);
 }
 
+/*
+ * Annotate our height from each node.
+ */
+static int
+annotate(struct ref *ref, size_t height, size_t colour)
+{
+	struct field	*f;
+	struct strct	*p;
+
+	if (ref->target->parent->colour == colour) {
+		warnx("loop in assignment: %s.%s -> %s.%s",
+			ref->source->parent->name,
+			ref->source->name,
+			ref->target->parent->name,
+			ref->target->name);
+		return(0);
+	}
+
+	p = ref->target->parent;
+	p->colour = colour;
+	p->height += height;
+	
+	TAILQ_FOREACH(f, &p->fq, entries)
+		if (FTYPE_REF == f->type)
+			if ( ! annotate(f->ref, height + 1, colour))
+				return(0);
+
+	return(1);
+}
+
+/*
+ * Sort by reverse height.
+ */
+static int
+parse_cmp(const void *a1, const void *a2)
+{
+	const struct strct 
+	      *p1 = *(const struct strct **)a1, 
+	      *p2 = *(const struct strct **)a2;
+
+	return((ssize_t)p1->height - (ssize_t)p2->height);
+}
+
 int
 parse_link(struct strctq *q)
 {
-	struct strct	*p;
-	struct field	*f;
+	struct strct	 *p;
+	struct strct	**pa;
+	struct field	 *f;
+	size_t		  colour = 1, sz = 0, i = 0;
+
+	/* First, establish linkage between nodes. */
 
 	TAILQ_FOREACH(p, q, entries)
 		TAILQ_FOREACH(f, &p->fq, entries) {
@@ -74,8 +122,41 @@ parse_link(struct strctq *q)
 			if ( ! checksource(f->ref, p) ||
 			     ! checktarget(f->ref, q))
 				return(0);
-			return(1);
 		}
 
-	return(0);
+	/* Now follow and order all outbound links. */
+
+	TAILQ_FOREACH(p, q, entries) {
+		sz++;
+		if (p->colour)
+			continue;
+		TAILQ_FOREACH(f, &p->fq, entries) {
+			if (FTYPE_REF != f->type)
+				continue;
+			if ( ! annotate(f->ref, 0, colour))
+				return(0);
+		}
+		colour++;
+	}
+
+	/* 
+	 * Copy the list into a temporary array.
+	 * Then sort the list by reverse-height.
+	 * Finally, re-create the list from the sorted elements.
+	 */
+
+	if (NULL == (pa = calloc(sz, sizeof(struct strct *))))
+		err(EXIT_FAILURE, NULL);
+	while (NULL != (p = TAILQ_FIRST(q))) {
+		TAILQ_REMOVE(q, p, entries);
+		assert(i < sz);
+		pa[i++] = p;
+	}
+	assert(i == sz);
+	qsort(pa, sz, sizeof(struct strct *), parse_cmp);
+	for (i = 0; i < sz; i++)
+		TAILQ_INSERT_HEAD(q, pa[i], entries);
+
+	free(pa);
+	return(1);
 }

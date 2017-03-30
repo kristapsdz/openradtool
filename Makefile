c4ddc8dba09cf7cfc36a9ce94e1ebae382e85cb3
kristapsdz <kristaps@bsd.lv>
2017-03-30 10:48:47 +0000

    Initial generation of SQL file.

--- Makefile
+++ Makefile
@@ -12,12 +12,13 @@ OBJS		 = header.o \
 		   linker.o \
 		   main.o \
 		   parser.o \
-		   source.o
+		   source.o \
+		   sql.o
 
 kwebapp: $(COMPAT_OBJS) $(OBJS)
 	$(CC) -o $@ $(COMPAT_OBJS) $(OBJS)
 
-test: db.o
+test: db.o db.db
 
 db.o: db.c db.h
 	$(CC) $(CFLAGS) -I/usr/local/include -o $@ -c db.c
@@ -28,12 +29,19 @@ db.c: kwebapp db.txt
 db.h: kwebapp db.txt
 	./kwebapp -h db.txt >$@
 
+db.sql: kwebapp db.txt
+	./kwebapp -s db.txt >$@
+
+db.db: db.sql
+	rm -f $@
+	sqlite3 $@ < db.sql
+
 $(COMPAT_OBJS) $(OBJS): config.h
 
 $(OBJS): extern.h
 
 clean:
-	rm -f kwebapp $(COMPAT_OBJS) $(OBJS) db.c db.h db.o
+	rm -f kwebapp $(COMPAT_OBJS) $(OBJS) db.c db.h db.o db.sql db.db
 
 distclean: clean
 	rm -f config.h config.log Makefile.configure
---
+++ sql.c
@@ -0,0 +1,70 @@
+#include "config.h"
+
+#include <sys/queue.h>
+
+#if HAVE_ERR
+# include <err.h>
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "extern.h"
+
+static void
+gen_fkeys(const struct field *f, int *first)
+{
+
+	if (FTYPE_STRUCT == f->type || NULL == f->ref)
+		return;
+
+	printf("%s\n\tFOREIGN KEY(%s) REFERENCES %s(%s)",
+		*first ? "" : ",",
+		f->ref->source->name,
+		f->ref->target->parent->name,
+		f->ref->target->name);
+	*first = 0;
+}
+
+static void
+gen_field(const struct field *f, int *first)
+{
+
+	switch (f->type) {
+	case (FTYPE_INT):
+		printf("%s\n\t%s INTEGER", 
+			*first ? "" : ",", f->name);
+		*first = 0;
+		break;
+	case (FTYPE_TEXT):
+		printf("%s\n\t%s TEXT", 
+			*first ? "" : ",", f->name);
+		*first = 0;
+		break;
+	default:
+		break;
+	}
+}
+
+static void
+gen_struct(const struct strct *p)
+{
+	const struct field *f;
+	int	 first = 1;
+
+	printf("CREATE TABLE %s (", p->name);
+	TAILQ_FOREACH(f, &p->fq, entries)
+		gen_field(f, &first);
+	TAILQ_FOREACH(f, &p->fq, entries)
+		gen_fkeys(f, &first);
+	puts("\n);\n"
+	     "");
+}
+
+void
+gen_sql(const struct strctq *q)
+{
+	const struct strct *p;
+
+	TAILQ_FOREACH(p, q, entries)
+		gen_struct(p);
+}

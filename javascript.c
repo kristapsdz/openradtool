26731c8a46a80c878013f86a1c5c92c9977f4345
kristaps <>
2017-05-19 16:40:13 +0000

    Documentation.

--- javascript.c
+++ javascript.c
@@ -29,10 +29,54 @@
 #include "extern.h"
 
 static void
-gen_js_field(const struct field *f)
+gen_jsdoc_field(const struct field *f)
 {
 
-	/* TODO: blob handling. */
+	if (FIELD_NOEXPORT & f->flags || FTYPE_BLOB == f->type)
+		return;
+
+	if (FIELD_NULL & f->flags) {
+		if (FTYPE_STRUCT == f->type) {
+			print_commentv(2, COMMENT_JS_FRAG,
+				"%s-%s-obj: invoke %s.fill() method "
+				"with %s data (if non-null)",
+				f->parent->name, f->name, 
+				f->ref->tstrct, f->name);
+		} else {
+			print_commentv(2, COMMENT_JS_FRAG,
+				"%s-%s-text: replace contents "
+				"with %s data (if non-null)",
+				f->parent->name, f->name, f->name);
+		}
+		print_commentv(2, COMMENT_JS_FRAG,
+			"%s-has-%s: \"hide\" class "
+			"removed if %s not null, otherwise "
+			"\"hide\" class is added",
+			f->parent->name, f->name, f->name);
+		print_commentv(2, COMMENT_JS_FRAG,
+			"%s-no-%s: \"hide\" class "
+			"added if %s not null, otherwise "
+			"\"hide\" class is removed",
+			f->parent->name, f->name, f->name);
+	} else {
+		if (FTYPE_STRUCT == f->type) {
+			print_commentv(2, COMMENT_JS_FRAG,
+				"%s-%s-obj: invoke %s.fill() method "
+				"with %s data",
+				f->parent->name, f->name, 
+				f->ref->tstrct, f->name);
+		} else {
+			print_commentv(2, COMMENT_JS_FRAG,
+				"%s-%s-text: replace contents "
+				"with %s data",
+				f->parent->name, f->name, f->name);
+		}
+	}
+}
+
+static void
+gen_js_field(const struct field *f)
+{
 
 	if (FIELD_NOEXPORT & f->flags || FTYPE_BLOB == f->type)
 		return;
@@ -52,8 +96,9 @@ gen_js_field(const struct field *f)
 		if (FTYPE_STRUCT == f->type) 
 			printf("\t\t\t\tlist = e.getElementsByClassName"
 				     "('%s-%s-obj');\n"
+			       "\t\t\tstrct = new %s(this.obj.%s);\n"
 			       "\t\t\tfor (i = 0; i < list.length; i++)\n"
-			       "\t\t\t\t%s.fill(list[i], this.obj.%s);\n",
+			       "\t\t\t\tstrct.fill(list[i]);\n",
 			       f->parent->name, f->name, 
 			       f->ref->tstrct, f->name);
 		else
@@ -65,8 +110,9 @@ gen_js_field(const struct field *f)
 		if (FTYPE_STRUCT == f->type) 
 			printf("\t\t\tlist = e.getElementsByClassName"
 				     "('%s-%s-obj');\n"
+			       "\t\t\tstrct = new %s(this.obj.%s);\n"
 			       "\t\t\tfor (i = 0; i < list.length; i++)\n"
-			       "\t\t\t\t%s.fill(list[i], this.obj.%s);\n",
+			       "\t\t\t\tstrct.fill(list[i]);\n",
 			       f->parent->name, f->name, 
 			       f->ref->tstrct, f->name);
 		else 
@@ -144,17 +190,35 @@ gen_javascript(const struct strctq *sq)
 	     "");
 
 	TAILQ_FOREACH(s, sq, entries) {
+		print_commentv(1, COMMENT_JS,
+			"Represent a \"%s\" object for filling "
+			"into a DOM tree.\n"
+			"@constructor\n"
+			"@param {Object} obj - The %s object.",
+			s->name, s->name);
 		printf("\tfunction %s(obj)\n"
 		       "\t{\n"
-		       "\t\tthis.obj = obj;\n"
-		       "\t\tthis.fill = function(e){\n",
+		       "\t\tthis.obj = obj;\n",
 		       s->name);
+		print_commentv(2, COMMENT_JS_FRAG_OPEN,
+			"Fill in a \"%s\" object at the given "
+			"element in the DOM tree.\n"
+			"Elements having the following classes "
+			"are manipulated as follows:",
+			s->name);
+		TAILQ_FOREACH(f, &s->fq, entries)
+			gen_jsdoc_field(f);
+		print_commentt(2, COMMENT_JS_FRAG_CLOSE,
+			"@param {Object} e - The DOM element.");
+		puts("\t\tthis.fill = function(e){");
 		TAILQ_FOREACH(f, &s->fq, entries)
 			if ( ! (FIELD_NOEXPORT & f->flags) &&
 			    FTYPE_STRUCT == f->type) {
-				puts("\t\t\tvar list, i;");
+				puts("\t\t\tvar list, strct, i;");
 				break;
 			}
+		puts("\t\t\tif (null === this.obj || null === e)\n"
+		     "\t\t\t\treturn;");
 		TAILQ_FOREACH(f, &s->fq, entries)
 			gen_js_field(f);
 		printf("\t\t};\n"

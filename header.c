4fc6f1131c06b0f8e9595abb9c5723107786536c
kristapsdz <kristaps@bsd.lv>
2018-08-28 08:20:46 +0000

    Catch allocation failure.

--- header.c
+++ header.c
@@ -836,6 +836,8 @@ main(int argc, char *argv[])
 
 	if (confsz > 0) {
 		confs = calloc(confsz, sizeof(FILE *));
+		if (NULL == confs)
+			err(EXIT_FAILURE, NULL);
 		for (i = 0; i < confsz; i++) {
 			confs[i] = fopen(argv[i], "r");
 			if (NULL == confs[i]) {

704d23b297784c20781618c1b7e2905bdd57e966
kristapsdz <kristaps@bsd.lv>
2020-08-05 22:07:13 +0000

    If passed as [], tsc will decompose it into null.  Explicitly allocate.

--- regress/javascript/simple-array-empty.ts
+++ regress/javascript/simple-array-empty.ts
@@ -1,6 +1,6 @@
 function runTest(dom: HTMLDocument): HTMLDocument
 {
-	const obj: ort.foo = new ort.foo([]);
+	const obj: ort.foo = new ort.foo(new Array<ort.foo>());
 
 	obj.fillArray(dom.getElementById('foo'));
 

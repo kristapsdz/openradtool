5569f15f3735a10bd42598b30916cbc60398192e
kristapsdz <kristaps@bsd.lv>
2020-08-08 09:57:13 +0000

    Test for undefined behaviour.

---
+++ regress/javascript/fill-bits-checked-undefined.ts
@@ -0,0 +1,11 @@
+function runTest(dom: HTMLDocument): HTMLDocument
+{
+	const data: ort.fooData = { 
+		'id': 1,
+	};
+	const obj: ort.foo = new ort.foo(data);
+
+	obj.fill(dom.body);
+
+	return dom;
+}

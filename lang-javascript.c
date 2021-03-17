93928c72797aad5b033c08d569cd1e405c9b217c
kristapsdz <kristaps@bsd.lv>
2021-03-17 21:30:43 +0000

    Remove unnecessary inclusions.

--- lang-javascript.c
+++ lang-javascript.c
@@ -19,16 +19,13 @@
 #if HAVE_SYS_QUEUE
 # include <sys/queue.h>
 #endif
-#include <sys/param.h>
 
 #include <assert.h>
-#include <fcntl.h>
 #include <inttypes.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #include "ort.h"
 #include "ort-lang-javascript.h"

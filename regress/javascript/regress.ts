8adf7126f70856be3d487ed9d4e7c38f73451d02
Kristaps Dz <kristaps@bsd.lv>
2024-08-25 12:44:31 -0700

    Upgrade dependencies and fix up JS regress.

--- regress/javascript/regress.ts
+++ regress/javascript/regress.ts
@@ -1,7 +1,9 @@
-const contents: Buffer =
-	fs.readFileSync(fname);
-const dom: JSDOM =
-	new JSDOM(contents.toString());
+const contents = fs.readFileSync(fname);
+const dom = new JSDOM(contents.toString());
+
+/* Fixup to pretend this is a browser. */
 
 (<any>global).document = dom.window.document;
+(<any>global).HTMLElement = dom.window.HTMLElement;
+
 return runTest(dom.window.document).body.innerHTML;
